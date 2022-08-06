08219e4c029a96bd44bcae2870d6eff24c49e511
mitrofanzxc <mitrofanzxc@gmail.com>
2022-08-06 17:30:47 +0300

    feat: add logic for progress bar (Sat, Aug  6, 2022  5:30:47 PM)

--- src/components/progressBar/ProgressBar.tsx
+++ src/components/progressBar/ProgressBar.tsx
@@ -14,7 +14,7 @@ const ProgressBar: FC<IProgressBar> = ({ header, description, fw, id, progress }
 
   useEffect(() => {
     const progressBar = document.querySelector(`.progress-ring__circle${id}`) as HTMLDivElement;
-    // const dot = document.querySelector(`.dot${id}`) as HTMLDivElement;
+    const dot = document.querySelector(`.dot${id}`) as HTMLDivElement;
     let progressValue = 0;
     // const speed = 50;
     // const radius = 132;
@@ -36,11 +36,15 @@ const ProgressBar: FC<IProgressBar> = ({ header, description, fw, id, progress }
     };
 
     tick();
+
+    setTimeout(() => {
+      dot.classList.add('opacity_one');
+    }, 1000);
   }, []);
 
   return (
     <div className={`circle progress-ring__circle${id}`}>
-      {/* <div className={`dot dot${id}`} /> */}
+      <div className={`dot dot${id} opacity_zero`} />
       <h2 className="color_orange">{header}</h2>
       <p className={`progress-bar__description ${fw}`}>{description}</p>
     </div>
