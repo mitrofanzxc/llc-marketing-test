e590386925b6a052fe0cad9ddb3208b3862499bb
mitrofanzxc <mitrofanzxc@gmail.com>
2022-08-08 13:28:11 +0300

    feat: add swiper to reviews section (Mon, Aug  8, 2022  1:28:11 PM)

--- src/sections/reviews/SectionReviews.tsx
+++ src/sections/reviews/SectionReviews.tsx
@@ -1,10 +1,9 @@
-import { FC, useRef, useState } from 'react';
+import { FC } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-
 import review1 from '../../images/review-1.png';
 import review2 from '../../images/review-2.png';
 import './SectionReviews.scss';
@@ -25,7 +24,7 @@ const SectionReviews: FC = () => {
           mousewheel={true}
           keyboard={true}
           slidesPerView={2}
-          spaceBetween={200}
+          spaceBetween={157}
           modules={[Navigation, Pagination, Mousewheel, Keyboard]}
           className="mySwiper"
         >
@@ -57,41 +56,7 @@ const SectionReviews: FC = () => {
               <p>Доволен работой и профессионализмом компании!</p>
             </div>
           </SwiperSlide>
-          <SwiperSlide className="review-slider__item">
-            <img src={review1} alt="review1" className="review-slider__item-img" />
-            <div className="review-slider__item-description color_blue">
-              <p>Спасибо компании за установку канализаций. Сделали быстро и на совесть!</p>
-              <p>Еще раз огромное спасибо и хороших клиентов!</p>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="review-slider__item">
-            <img src={review2} alt="review2" className="review-slider__item-img" />
-            <div className="review-slider__item-description color_blue">
-              <p>Обратился за помощью в установке канализации в новый дом за Минском.</p>
-              <p>Доволен работой и профессионализмом компании!</p>
-            </div>
-          </SwiperSlide>
         </Swiper>
-        {/* <div className="reviews-slider__wrapper">
-          <div className="reviews-slider__button left" />
-          <div className="review-slider">
-            <div className="review-slider__item">
-              <img src={review1} alt="review1" className="review-slider__item-img" />
-              <div className="review-slider__item-description color_blue">
-                <p>Спасибо компании за установку канализаций. Сделали быстро и на совесть!</p>
-                <p>Еще раз огромное спасибо и хороших клиентов!</p>
-              </div>
-            </div>
-            <div className="review-slider__item">
-              <img src={review2} alt="review2" className="review-slider__item-img" />
-              <div className="review-slider__item-description color_blue">
-                <p>Обратился за помощью в установке канализации в новый дом за Минском.</p>
-                <p>Доволен работой и профессионализмом компании!</p>
-              </div>
-            </div>
-          </div>
-          <div className="reviews-slider__button right" />
-        </div> */}
       </div>
     </section>
   );
