2d19a77d9bb30493877b09d47b6724a9eeb94ed4
mitrofanzxc <mitrofanzxc@gmail.com>
2022-08-12 10:53:22 +0300

    refactor: change the structure of the project (Fri, Aug 12, 2022 10:53:22 AM)

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import { App } from './App';
-import { ScrollToTop } from './features/scrollToTop';
 import './index.scss';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
@@ -13,7 +12,6 @@ const root = createRoot(container);
 root.render(
   <Provider store={store}>
     <HashRouter>
-      <ScrollToTop />
       <App />
     </HashRouter>
   </Provider>
