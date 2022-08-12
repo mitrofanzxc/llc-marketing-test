d3c0bd2743ded6e9536469784a5c5fd88f36716e
mitrofanzxc <mitrofanzxc@gmail.com>
2022-08-12 12:03:21 +0300

    feat: add swiper (Fri, Aug 12, 2022 12:03:21 PM)

--- src/sections/examples/SectionExamples.tsx
+++ src/sections/examples/SectionExamples.tsx
@@ -5,6 +5,12 @@ import workExample3 from '../../images/work-example-3.jpg';
 import workExample4 from '../../images/work-example-4.jpg';
 import './SectionExamples.scss';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 const SectionExamples: FC = () => {
   return (
     <section className="examples">
@@ -13,18 +19,46 @@ const SectionExamples: FC = () => {
           <strong>Примеры</strong> наших работ
         </h2>
         <div className="slider">
-          <div className="slide">
-            <img src={workExample1} alt="workExample1" />
-          </div>
-          <div className="slide">
-            <img src={workExample2} alt="workExample2" />
-          </div>
-          <div className="slide">
-            <img src={workExample3} alt="workExample3" />
-          </div>
-          <div className="slide">
-            <img src={workExample4} alt="workExample4" />
-          </div>
+          <Swiper
+            cssMode={true}
+            navigation={true}
+            pagination={{
+              clickable: true,
+            }}
+            mousewheel={true}
+            keyboard={true}
+            slidesPerView={4}
+            spaceBetween={40}
+            modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+            breakpoints={{
+              769: {
+                slidesPerView: 4,
+              },
+              768: {
+                slidesPerView: 2,
+              },
+              361: {
+                slidesPerView: 2,
+              },
+              360: {
+                slidesPerView: 1,
+              },
+            }}
+            className="mySwiper"
+          >
+            <SwiperSlide className="slide">
+              <img src={workExample1} alt="workExample1" />
+            </SwiperSlide>
+            <SwiperSlide className="slide">
+              <img src={workExample2} alt="workExample2" />
+            </SwiperSlide>
+            <SwiperSlide className="slide">
+              <img src={workExample3} alt="workExample3" />
+            </SwiperSlide>
+            <SwiperSlide className="slide">
+              <img src={workExample4} alt="workExample4" />
+            </SwiperSlide>
+          </Swiper>
         </div>
       </div>
     </section>
